a9b78cb8202a46be2cf0b3f8e2cca7f034519a17
Shailysahu2 <shailysahu2003@gmail.com>
2025-06-24 21:45:30 +0530

    env update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api' // Change to your backend URL
+  apiUrl: 'https://sneh-backend.onrender.com/api'
+  // Change to your backend URL
 };
