8078307a3e34119948685f41025ec4eecd0af307
Shailysahu2 <shailysahu2003@gmail.com>
2025-08-17 22:26:02 +0530

    c

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://sneh-backend-b8q4.onrender.com/api',
-  // apiUrl: 'http://localhost:3000/api'
+    // apiUrl: 'http://localhost:3000/api',
+     apiUrl: 'https://sneh-backend-b8q4.onrender.com/api'
+    
 };
